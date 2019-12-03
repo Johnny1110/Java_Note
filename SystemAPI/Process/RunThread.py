306d51ba9c1d110cc3c3ab3e1294dcdafa237b0f
Johnny <Jarvan1110@gmail.com>
2019-12-03 14:57:27 +0800

    finished CMDCaller --sec

--- SystemAPI/Process/RunThread.py
+++ SystemAPI/Process/RunThread.py
@@ -17,6 +17,7 @@ def innerErr(err_queue):
 
 
 def throwInnerErr(err_queue):
+    # 執行緒中緒
     inner_t = Thread(target=innerErr, args=(err_queue,))
     inner_t.start()
 
@@ -24,13 +25,17 @@ def throwInnerErr(err_queue):
 
 def start():
     print('into start func')
+
+    # 建立 err_queue 來接所有執行緒錯誤
     err_queue = queue.Queue()
+    # 注意傳入的參數 args 必須是 Iterator : args=(err_queue,) 
     t1 = Thread(target=throwErr, args=(err_queue,))
     t2 = Thread(target=throwInnerErr, args=(err_queue,))
 
     t1.start()
     t2.start()
 
+    # 檢查 queue 裡面是否有 Exception 出現
     while True:
         try:
             err = err_queue.get(block=False)
@@ -39,9 +44,10 @@ def start():
         else:
             print('raise ERROR in queue')
             raise err
-
+        
+        # 防阻塞
         t1.join(0.1)
-        t1.join(0.2)
+        t1.join(0.1)
         if(t1.isAlive() or t2.isAlive()):
             continue
         else:
@@ -51,6 +57,6 @@ def start():
 
 
 if __name__ == '__main__':
-    print('runing start ...')
+    print('run start() ...')
     start()
-    print('start end ...')
+    print('end start() ...')
